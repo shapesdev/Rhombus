7c822f5122c4df6e604481f80989054841307c64
Shapes <shapesgames@gmail.com>
2023-07-18 14:18:53 +1000

    Add start points to array
    Add start points and draw circles in canvas for them

--- src/index.js
+++ src/index.js
@@ -4,14 +4,18 @@ canvas.style='border:2px solid #000000';
 document.body.appendChild(canvas);
 let ctx = canvas.getContext('2d');
 
-let canvasWidth = 600;
-let canvasHeight = 600;
-
+let canvasWidth = 700;
+let canvasHeight = 700;
+ 
 let gridSize = 5;
 let cellSize = 100;
+let offset = 100;
+
+let points = [];
 
 resize();
 drawGrid();
+drawPoints();
 
 function resize() {
     ctx.canvas.width = canvasWidth;
@@ -24,10 +28,23 @@ function drawGrid() {
     ctx.strokeStyle = '#000000';
     ctx.beginPath();
 
-    for(let i = 0; i < gridSize; i++) {
-        for(let j = 0; j < gridSize; j++) {
-            ctx.rect(i * cellSize, j * cellSize, cellSize, cellSize);
+    for(let i = 0; i <= gridSize; i++) {
+        for(let j = 0; j <= gridSize; j++) {
+            if(i < gridSize && j < gridSize) {
+                ctx.rect(i * cellSize + offset, j * cellSize + offset, cellSize, cellSize);
+            }
+            points.push({x:i * cellSize + offset, y: j * cellSize + offset});
         }
     }
     ctx.stroke();
+}
+
+function drawPoints() {
+    ctx.fillStyle = '#11EE28';
+    points.forEach((point) => {
+        ctx.beginPath();
+        ctx.arc(point.x, point.y, 8, 0, 2 * Math.PI);
+        ctx.fill();
+        ctx.stroke();
+    })
 }
