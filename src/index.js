aaf64fd774ac5b0df3dcad8297907040878725b5
Shapes <shapesgames@gmail.com>
2023-07-18 13:12:55 +1000

    Create Canvas in script
    Creating Canvas in script instead of having it in html file

--- src/index.js
+++ src/index.js
@@ -1,5 +1,7 @@
-var canvas = document.getElementById('canvas');
+let canvas = document.createElement('canvas');
+canvas.id = 'canvas';
+canvas.style='border:2px solid #000000';
 document.body.appendChild(canvas);
-var ctx = canvas.getContext('2d');
+let ctx = canvas.getContext('2d');
 
 console.log(canvas.id);
