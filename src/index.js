6c310a35fee39539234688424be62fef77402ab4
Shapes <shapesgames@gmail.com>
2023-07-18 13:20:53 +1000

    Draw a cube in Canvas

--- src/index.js
+++ src/index.js
@@ -8,8 +8,18 @@ let canvasWidth = 500;
 let canvasHeight = 500;
 
 resize();
+drawCube();
 
 function resize() {
     ctx.canvas.width = canvasWidth;
     ctx.canvas.height = canvasHeight;
+}
+
+function drawCube() {
+    ctx.lineWidth = 1;
+    ctx.lineCap = 'round';
+    ctx.strokeStyle = '#BBBBBB';
+    ctx.beginPath();
+    ctx.rect(0, 0, 100, 100);
+    ctx.stroke();
 }
