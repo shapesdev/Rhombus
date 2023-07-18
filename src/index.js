95595aaf138e1aea5207b6fb33493946073eb496
Shapes <shapesgames@gmail.com>
2023-07-18 14:45:42 +1000

    Draw grid with Path2D
    Better for pathfinding and actually claiming the territories, since we can simply fill or add any stuff in the path itself

--- src/index.js
+++ src/index.js
@@ -5,8 +5,8 @@ canvas.style='border:2px solid #000000';
 document.body.appendChild(canvas);
 let ctx = canvas.getContext('2d');
 
-let canvasWidth = 700;
-let canvasHeight = 700;
+let canvasWidth = 500;
+let canvasHeight = 500;
 
 // Grid
 let gridSize = 5;
@@ -19,46 +19,34 @@ let points = [];
 document.addEventListener('mousedown', getClosestPoint);
 
 resize();
-drawGrid();
-drawPoints();
+drawGridWithPath2D();
 
 function resize() {
     ctx.canvas.width = canvasWidth;
     ctx.canvas.height = canvasHeight;
 }
 
-function drawGrid() {
-    ctx.lineWidth = 2;
-    ctx.lineCap = 'round';
-    ctx.strokeStyle = '#000000';
-    ctx.beginPath();
+function drawGridWithPath2D() {
+    for(let i = 0; i < gridSize; i++) {
+        for(let j = 0; j < gridSize; j++) {
+            const path = new Path2D();
+            path.rect(i * cellSize, j * cellSize, cellSize, cellSize);
+            path.closePath();
 
-    for(let i = 0; i <= gridSize; i++) {
-        for(let j = 0; j <= gridSize; j++) {
-            if(i < gridSize && j < gridSize) {
-                ctx.rect(i * cellSize + offset, j * cellSize + offset, cellSize, cellSize);
-            }
-            points.push({x:i * cellSize + offset, y: j * cellSize + offset});
+            ctx.fillStyle = '#39C738';
+            ctx.fill(path);
+            ctx.lineWidth = 2;
+            ctx.strokeStyle = '#000000';
+            ctx.stroke(path);
         }
     }
-    ctx.stroke();
-}
-
-function drawPoints() {
-    ctx.fillStyle = '#11EE28';
-    points.forEach((point) => {
-        ctx.beginPath();
-        ctx.arc(point.x, point.y, 8, 0, 2 * Math.PI);
-        ctx.fill();
-        ctx.stroke();
-    })
 }
 
 function getClosestPoint(e) {
     let tempPos = {x: 0, y: 0};
     tempPos.x = e.x;
     tempPos.y = e.y;
-    
+
     let minDistance = 1000000;
     let closestPoint;
 
